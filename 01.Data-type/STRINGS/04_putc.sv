fdec987648fd2a78fae1927607b236c6eb1329cc
JAYA SOUNDHAR V T <148468173+jayasoundhar@users.noreply.github.com>
2025-07-06 23:28:18 +0530

    Update 04_putc.sv

--- 01.Data-type/STRINGS/04_putc.sv
+++ 01.Data-type/STRINGS/04_putc.sv
@@ -1,14 +1,8 @@
 module str_04;
   string A=("DESIGN_ENGINNER");
   initial begin
-    $display("your's out str_04 on A=%s",A.putc(5,"d"));
+    $display("A=%s",A.putc(5,"d"));
   end
 endmodule
 
-result
-EDA complier's tool error for put.c keyword
-ERROR VCP7292 "String method putc is not a function." 
-
-  EXPECTED OUTPUT 
-   "DESIGD_ENGINNER"
-  
+// DESIdN_ENGINNER
