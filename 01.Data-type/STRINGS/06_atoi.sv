bd2f1fd7de7ec6e4f4b850191f7b8dcd3fe16322
JAYA SOUNDHAR V T <148468173+jayasoundhar@users.noreply.github.com>
2025-07-07 11:00:56 +0530

    Update 06_atoi.sv

--- 01.Data-type/STRINGS/06_atoi.sv
+++ 01.Data-type/STRINGS/06_atoi.sv
@@ -1,8 +1,14 @@
 module str_06;
   string A=("3455647RING");
+  int b;
   initial begin
+    b = A.atoi();
     $display("A=%d",A.atoi());
+    $display("A=%d",A);
+    $display("A=%d",b);
   end
 endmodule
 
-// 3455647
+// A=    3455647
+// A=3455647RING
+// A=    3455647
