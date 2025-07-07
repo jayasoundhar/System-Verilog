86a9b9c2c7e7539852082e76a57a6319a1b6fad9
JAYA SOUNDHAR V T <148468173+jayasoundhar@users.noreply.github.com>
2025-07-07 15:37:41 +0530

    Update and rename 01.sv to 01_len.sv

--- 01.Data-type/STRINGS/01_len.sv
+++ 01.Data-type/STRINGS/01_len.sv
@@ -1,10 +1,9 @@
 module str_1;
   string A="DESIGN ENGINEER";
   initial begin
-    $display("output A=%d",A.len);
+    $display("A=%d",A.len);
   end
 endmodule
 
 
-result output_str_1
-KERNEL: output A= 15
+// A= 15
