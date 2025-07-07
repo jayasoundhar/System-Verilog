684a198352a08484edc9350f143c2673a2c1fe31
JAYA SOUNDHAR V T <148468173+jayasoundhar@users.noreply.github.com>
2025-07-07 15:38:20 +0530

    Update 02.sv

--- 01.Data-type/STRINGS/02.sv
+++ 01.Data-type/STRINGS/02.sv
@@ -1,9 +1,8 @@
 module str_02;
   string A =("design_engineer");
   initial begin
-  $display("yours output on A=%s ",A.toupper());
+  $display("A=%s ",A.toupper());
   end
 endmodule
 
-results
-KERNEL: yours output's str_02 on A=DESIGN_ENGINEER
+// A=DESIGN_ENGINEER
