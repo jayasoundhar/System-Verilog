b88f0a7748449b6c91e1fc1e3766e9b85dcbc627
JAYA SOUNDHAR V T <148468173+jayasoundhar@users.noreply.github.com>
2025-07-07 14:24:34 +0530

    Update and rename str_14.sv to 14_print.sv

---
+++ 01.Data-type/STRINGS/14_print.sv
@@ -0,0 +1,27 @@
+module str_14;
+  string A=("DESIGN VERIFICATION");
+  initial begin
+    for(int i=0; i<18;i++)
+      $display("A%s",A[i]);
+  end
+endmodule
+
+
+// A =>D
+// A =>E
+// A =>S
+// A =>I
+// A =>G
+// A =>N
+// A => 
+// A =>V
+// A =>E
+// A =>R
+// A =>I
+// A =>F
+// A =>I
+// A =>C
+// A =>A
+// A =>T
+// A =>I
+// A =>O
