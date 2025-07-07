6b1be7e65d5e0fc80fe27c2d1673cd54df47d78e
JAYA SOUNDHAR V T <148468173+jayasoundhar@users.noreply.github.com>
2025-07-07 12:44:38 +0530

    Update and rename str_12.sv to 12_equal.sv

--- 01.Data-type/STRINGS/12_equal.sv
+++ 01.Data-type/STRINGS/12_equal.sv
@@ -1,13 +1,12 @@
 module str_12;
   string A="DESIGN";
-  string B="DESIGN;
+  string B="DESIGN";
   initial begin
     if(A==B)
       $display("A and B are equal");
       else
-        $display("Aand B are not equal");
+        $display("A and B are not equal");
   end
 endmodule
 
-result
-# KERNEL: A and B are equal
+// A and B are equal
